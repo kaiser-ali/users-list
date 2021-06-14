570da18adaddf6807227803f86c7739008d5d507
md kaiser ali <mdaryanli34@gmail.com>
2021-06-14 14:10:41 +0530

    initial commit

---
+++ src/Posts.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Posts = ({posts, loading}) => {
+    if(loading){
+        return <h1>Loading...</h1>;
+    }
+    return(
+        <div>
+            <ul className="list-group mb-4">
+                {posts.map(post => (
+                    <li key={post.id} className="list-group-item">
+                        {post.title}
+                    </li>
+
+                )
+                
+                )
+
+                }
+
+            </ul>
+
+        </div>
+    );
+
+}
+
+export default Posts;
